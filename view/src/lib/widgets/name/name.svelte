2936b28cba00e3a600877c1d5cbf3b323ef74382
Josh <43808099+josh-codes@users.noreply.github.com>
2021-08-28 16:41:50 +0800

    I forgot a day in the week and also i splet one wrong 🤦🏻‍♂️

--- view/src/lib/widgets/name/name.svelte
+++ view/src/lib/widgets/name/name.svelte
@@ -5,14 +5,14 @@
         a6: "Good morning",
         a12: "Good evening"
     }
-    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saterday"];
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let time = new Date();
     setInterval(()=>{
         time = new Date();
     },1000)
 </script>
-<h2>{ time.getHours() >= 18?  data.p6 : time.getHours() >= 12? data.a12 :  time.getHours()>= 6? data.a6 : time.getHours() >= 0 &&  data.p6 } { data.name }.<br> Today is { weekdays[ time.getDay()-1 ] } the { time.getDate() }th, { months[time.getMonth()] } { time.getFullYear() }.</h2>
+<h2>{ time.getHours() >= 18?  data.p6 : time.getHours() >= 12? data.a12 :  time.getHours()>= 6? data.a6 : time.getHours() >= 0 &&  data.p6 } { data.name }.<br> Today is { weekdays[ time.getDay() ] } the { time.getDate() }th, { months[time.getMonth()] } { time.getFullYear() }.</h2>
 <style lang="scss">
     h2 {
         color: white;
